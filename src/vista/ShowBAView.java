ed728e36c774daea813b9048f5185d687ff49a15
Manu Hurtado <guimhur@gmail.com>
2019-10-12 16:29:31 +0200

    feat(): create show bien amortizable view class

---
+++ src/vista/ShowBAView.java
@@ -0,0 +1,26 @@
+package vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class ShowBAView extends JDialog implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShowBAView(JFrame parent/* , ShowBAController showBAController */) {
+		super(parent);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
